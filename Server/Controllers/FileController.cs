77b50727d6f5aa133ee5049c538c29efbd85f3c1
JP Dillingham <jp@dillingham.me>
2022-10-01 08:41:51 -0500

    failed experiment

--- Server/Controllers/FileController.cs
+++ Server/Controllers/FileController.cs
@@ -1,25 +1,78 @@
-using System.IO;
+using System.Collections.Concurrent;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Server.Hubs;
 
 namespace Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class FileController : ControllerBase
-    {
-        private readonly ILogger<FileController> _logger;
-
-        public FileController(ILogger<FileController> logger)
+    {   
+        public FileController(IHubContext<LinkHub> hub)
         {
-            _logger = logger;
+            Hub = hub;
         }
 
+        private IHubContext<LinkHub> Hub { get; }
+        private static ConcurrentDictionary<string, TaskCompletionSource<Stream>> Streams { get; } = new();
+
         [HttpGet("{filename}")]
-        public IActionResult Get(string filename)
+        public async Task<IActionResult> Get(string filename)
         {
-            Console.WriteLine($"Fetching {filename}");
-            var stream = new FileStream(Path.Combine(@"C:\slsk-downloads", filename), FileMode.Open, FileAccess.Read);
-            return File(stream, "application/octet-stream");
+            Console.WriteLine($"Requesting {filename} from link");
+            await Hub.SendRequest(filename);
+
+            var tcs = new TaskCompletionSource<Stream>();
+
+            if (Streams.TryAdd(filename, tcs))
+            {
+                try
+                {
+                    Console.WriteLine($"Added {filename} record. Waiting on file from link...");
+                    var task = await Task.WhenAny(tcs.Task, Task.Delay(5000));
+
+                    if (task == tcs.Task)
+                    {
+                        Console.WriteLine("Got the stream! Trying to write it back out...");
+                        var stream = await tcs.Task;
+                        return File(stream, "application/octet-stream");
+                    }
+                    else
+                    {
+                        throw new TimeoutException("link didn't respond in a timely manner");
+                    }
+                }
+                finally
+                {
+                    Streams.TryRemove(filename, out _);
+                }
+            }
+            else
+            {
+                throw new Exception("already waiting for this file!");
+            }
+        }
+
+        [HttpPost("{filename}")]
+        public IActionResult Post(string filename)
+        {
+            Console.WriteLine($"File POSTed: {filename}");
+
+            var stream = Request.Form.Files.First().OpenReadStream();
+
+            if (Streams.TryGetValue(filename, out var tcs))
+            {
+                Console.WriteLine("Setting result with stream...");
+                tcs.TrySetResult(stream);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+            return Ok();
         }
     }
 }
---
+++ Server/Hubs/LinkHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs
+{
+    public static class LinkHubMethods
+    {
+        public static readonly string Request = "REQUEST";
+    }
+
+    public static class LinkHubExtensions
+    {
+        public static Task SendRequest(this IHubContext<LinkHub> hub, string file)
+        {
+            return hub.Clients.All.SendAsync(LinkHubMethods.Request, file);
+        }
+    }
+
+    public class LinkHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Client {Clients.Caller} connected");
+            return Task.CompletedTask;
+        }
+    }
+}
